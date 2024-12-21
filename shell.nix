813b2448a8bad7d2d30d384e83bb158ef5f95561
PhilAtVisir <PhilAtVisir@Visir.ai>
2024-12-21 14:18:56 +1300

    update shell.nix and arch

--- shell.nix
+++ shell.nix
@@ -3,20 +3,20 @@
 let
   aws-signing-helper = pkgs.stdenv.mkDerivation {
     pname = "aws-signing-helper";
-    version = "1.3.0";
+    version = "1.4.0";
+    
     src = pkgs.fetchurl {
-      url = "https://github.com/PhilAtVisir/aws-signing-helper/releases/download/v1.3.0/rolesanywhere-credential-helper";
-      # You can get the hash by trying to build with an incorrect hash first
-      # Nix will tell you the correct hash
-      sha256 = "0aw3dicvz2wizc645cyxw6p53jah2gd67mxjjdf30qnh3hfn5hg7"; # Replace with actual hash from the release
+      url = "https://github.com/PhilAtVisir/aws-signing-helper/releases/download/v1.4.0/aws_signing_helper_x86_64-linux";
+      hash = null;  # Will show correct hash on first build attempt
     };
     
     dontUnpack = true;
+    nativeBuildInputs = [ pkgs.patchelf ];
     
     installPhase = ''
       mkdir -p $out/bin
-      cp $src $out/bin/aws-signing-helper
-      chmod +x $out/bin/aws-signing-helper
+      cp $src $out/bin/aws_signing_helper
+      chmod +x $out/bin/aws_signing_helper
     '';
   };
 in
